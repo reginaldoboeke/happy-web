b79b71f22869e8cb8147ce0c849d4de18a28c672
Reginaldo Boeke <reginaldoboeke@MacBook-Pro-de-Reginaldo.local>
2020-10-14 21:40:03 -0300

    Criadas landing e orphanage map

---
+++ src/pages/OrphanagesMap.tsx
@@ -0,0 +1,68 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import { Map, TileLayer, Marker, Popup } from 'react-leaflet';
+import Leaflet from 'leaflet';
+import { FiArrowRight, FiPlus } from 'react-icons/fi';
+
+import 'leaflet/dist/leaflet.css';
+
+import mapMarkerImg from '../images/map-marker.svg';
+
+import '../styles/pages/orphanages-map.css'
+
+const mapIcon = Leaflet.icon({
+  iconUrl: mapMarkerImg,
+
+  iconSize: [58, 68],
+  iconAnchor: [29, 68],
+  popupAnchor: [170, 2],
+});
+
+function OrphanagesMap() {
+  return (
+    <div id="page-map">
+      <aside>
+        <header>
+          <img src={mapMarkerImg} alt="Happy"/>
+
+          <h2>Escolha um orfanato no mapa</h2>
+          <p>Muitas crianças estão esperando a sua visita :)</p>
+        </header>
+
+        <footer>
+          <strong>São Gabriel da Palha</strong>
+          <span>Espírito Santo</span>
+        </footer>
+      </aside>
+
+      <Map
+        center={[-19.0339378,-40.5432299]}
+        zoom={15}
+        style={{ width: '100%', height: '100%' }}
+      >
+        {/* <TileLayer url="https://a.tile.openstreetmap.org/{z}/{x}/{y}.png" /> */}
+        <TileLayer
+          url={`https://api.mapbox.com/styles/v1/mapbox/light-v10/tiles/256/{z}/{x}/{y}@2x?access_token=${process.env.REACT_APP_MAPBOX_TOKEN}`}
+        />
+
+        <Marker
+          icon={mapIcon}
+          position={[-19.0339378,-40.5432299]}
+        >
+          <Popup closeButton={false} minWidth={240} maxWidth={240} className="map-popup">
+            Lar das meninas
+            <a href="#">
+              <FiArrowRight size={20} color="#fff" />
+            </a>
+          </Popup>
+        </Marker>
+      </Map>
+
+      <Link to="" className="create-orphanage">
+        <FiPlus size={32} color="#fff" /> 
+      </Link>
+    </div>
+  );
+}
+
+export default OrphanagesMap;
